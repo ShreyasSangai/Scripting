d4b9877431b66b6c46d493f84ba864875d5a8d10
SholayThakur <86546742+ShreyasSangai@users.noreply.github.com>
2026-02-26 03:26:08 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,1 @@
 echo "We are trying this practical from linux EC2"
-echo "1. this is first commit from master"
-
-echo "3. this is third commit from master"
-echo "2. this is second commit from feature"
