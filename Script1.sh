e7bea2d43934c421131833043e20ec5134f44a0d
SholayThakur <86546742+ShreyasSangai@users.noreply.github.com>
2026-02-26 02:30:48 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,3 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-31 making changes from Branch2 and learning git/gihub"
 echo "We are from batch-31 and learning git/gihub"
+echo "To check Fetch feature"
