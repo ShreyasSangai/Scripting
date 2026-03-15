edbc03ad5e753d54d10d3800794d8f6c12eb315d
Shreyas Sangai <sangaishreyas8@gmail.com>
2026-03-15 15:06:59 +0530

    This is commit in script 5

--- Script5.sh
+++ Script5.sh
@@ -5,3 +5,4 @@ readonly NAME
 NAME=DEVOPS
 echo "my name is: $NAME"
 echo "This is my first github code"
+#This script is to make variable read only, means we cannot set the value of NAME variable again
